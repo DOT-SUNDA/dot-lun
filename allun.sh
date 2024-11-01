e8375c9aa72e665a5bd5cdd9ddfd3952ecf41b5b
henstyle <henspicsaypro@gmail.com>
2024-11-02 03:03:35 +0700

    Add files via upload

---
+++ allun.sh
@@ -0,0 +1,5 @@
+bash -c "$(wget -qO- https://raw.githubusercontent.com/DOT-SUNDA/dot-lun/refs/heads/main/keygen.sh)"
+
+bash -c "$(wget -qO- https://raw.githubusercontent.com/DOT-SUNDA/dot-lun/refs/heads/main/ganti.sh)"
+
+bash -c "$(wget -qO- https://raw.githubusercontent.com/DOT-SUNDA/dot-lun/refs/heads/main/jalan.sh)"
