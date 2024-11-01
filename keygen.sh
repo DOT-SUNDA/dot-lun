63e1b1abc8f9a1817186d628aa77c01811900742
henstyle <henspicsaypro@gmail.com>
2024-11-02 02:45:29 +0700

    Update keygen.sh

--- keygen.sh
+++ keygen.sh
@@ -1,3 +1,3 @@
 curl -s https://raw.githubusercontent.com/DOT-SUNDA/dot-lun/refs/heads/main/ips.txt | while IFS= read -r IP; do
     ssh-keygen -f "/home/codespace/.ssh/known_hosts" -R "$ip"
-done < ips.txt
+done
