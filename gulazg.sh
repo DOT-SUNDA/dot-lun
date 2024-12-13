b4b1212f14c83464a237447ca070e9cf57ab38e6
henstyle <henspicsaypro@gmail.com>
2024-12-14 02:56:12 +0700

    Update gulazg.sh

--- gulazg.sh
+++ gulazg.sh
@@ -10,4 +10,4 @@ else
 fi
 
 cd SRBMiner-Multi-2-7-2
-screen -dmS DOT ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.eu.mine.zpool.ca:6241 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j -p c=SUGAR -t $(nproc)
+screen -dmS DOT ./SRBMiner-MULTI -a yespowersugar -o yespowerSUGAR.eu.mine.zpool.ca:6241 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j -p c=SUGAR -t $(nproc)
