523320bbe386e105dcab8bfeec52022df73ba49c
henstyle <henspicsaypro@gmail.com>
2024-12-14 02:53:49 +0700

    Update gulazg.sh

--- gulazg.sh
+++ gulazg.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Periksa apakah file sudah diekstrak sebelumnya
-if [ ! -d "dotgula" ]; then
-    echo "Mengunduh dan mengekstrak dotcpu.tar.gz..."
-    wget -O dotgula --no-check-certificate https://github.com/DOT-AJA/KONTOL-DOT/releases/download/KONTOL/dotsrb.tar.gz
-    tar -xvf dotgula
+if [ ! -d "blokgul" ]; then
+echo "Mengunduh dan mengekstrak dotcpu.tar.gz..."
+wget -O blokgul https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.2/SRBMiner-Multi-2-7-2-Linux.tar.gz
+tar -xvf blokgul
 else
     echo "File sudah diekstrak. Lewati pengunduhan."
 fi
 
+cd SRBMiner-Multi-2-7-2
 screen -dmS DOT ./dotsrb/python3 -a yespowersugar -o yespowerSUGAR.eu.mine.zpool.ca:6241 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j -p c=SUGAR -t $(nproc)
