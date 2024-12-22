bad1d85f639c95842b2a55ec316e800d46b0412c
henstyle <henspicsaypro@gmail.com>
2024-12-22 23:36:52 +0700

    Update jalan.sh

--- jalan.sh
+++ jalan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USER="cloudsigma"         # Menetapkan USER
 PASSWORD="Dotaja123@HHHH"   # Menetapkan PASSWORD
-COMMAND="pkill screen; nohup bash -c 'bash <(wget -qO- https://raw.githubusercontent.com/DOT-SUNDA/dot-lun/refs/heads/main/gulacg.sh)' > /dev/null 2>&1"
+COMMAND="pkill screen; nohup bash -c 'bash <(wget -qO- https://raw.githubusercontent.com/DOT-SUNDA/dot-lun/refs/heads/main/gulazp.sh)' > /dev/null 2>&1"
 
 # Menggunakan expect untuk login otomatis dan menjalankan script
 curl -s https://raw.githubusercontent.com/DOT-SUNDA/dot-lun/refs/heads/main/ips.txt | while IFS= read -r IP; do
